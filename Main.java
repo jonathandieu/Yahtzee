29db3c2e6e0ea783af75bdc66dda163df4cc36bf
jonathanhuydieu <jonathanhuydieu@knights.ucf.edu>
2020-10-25 01:21:01 -0400

    Implements Maxiyatzy, still needs to fix arraylist pPoints and getSum method

--- Main.java
+++ Main.java
@@ -1,10 +1,22 @@
 import java.util.*;
 
 class Main {
+
+
   public static void main(String[] args) 
   {
     Scanner sc = new Scanner(System.in);
     ArrayList<Integer> diceRoll = new ArrayList<Integer>();
+    ArrayList<Integer> sequentialDiceRoll = new ArrayList<Integer>(Arrays.asList(1,2,3,4,5,6));
+
+    System.out.println(sequentialDiceRoll);
+    
+    // Values that will be printed out at the end
+    int cPoints = 0;
+    int pPoints = 0;
+    int sStraight = 0;
+    int lStraight = 0;
+    int maxiYahtzy = 0;
 
     System.out.println("Please enter your dice roll");
     while(diceRoll.size() < 6)
@@ -20,12 +32,45 @@ class Main {
         }
     }
     
+    // Sorts the arraylist values
     Collections.sort(diceRoll);
     System.out.println(diceRoll);
 
-    
-    System.out.println(diceRoll.get(0));
 
 
+    int firstNum = diceRoll.get(0);
+    int lastNum = diceRoll.get(5);
+
+    if(firstNum == lastNum)
+    {
+        maxiYahtzy = 100;
+    }
+
+    else if(diceRoll.contains(sequentialDiceRoll))
+    {
+        pPoints = 35;
+    }
+    else
+    {
+        cPoints = getSum(diceRoll);
+    }
+
+    System.out.printf("Chance: %d points\n", cPoints);
+    System.out.printf("Three pairs: %d points\n", pPoints);
+    System.out.printf("Small Straight: %d points\n", sStraight);
+    System.out.printf("Large Straight: %d points\n", lStraight);
+    System.out.printf("Maxiyatzy: %d points\n", maxiYahtzy);
+
+  }
+  public static int getSum(ArrayList<Integer> someAL)
+  {
+    int sum = 0;
+    int size = someAL.size();
+
+    for(int i = 0; i < size; i++)
+    {
+        sum += someAL.get(i);
+    }
+    return sum;
   }
 }
